8aaa65f7ca9f7741e7d2891b3cd3bf269c25758d
NovartonA <133905469+Novarton@users.noreply.github.com>
2025-07-10 17:58:56 -0600

    Flujo de aplicacion

--- app/src/main/java/com/example/gastospersonales/activities/MainActivity.java
+++ app/src/main/java/com/example/gastospersonales/activities/MainActivity.java
@@ -8,19 +8,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
 import com.example.gastospersonales.R;
+import com.example.gastospersonales.auth.PinActivity;
+import com.example.gastospersonales.auth.SetPinActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        SharedPreferences prefs = getSharedPreferences("gastos_prefs", Context.MODE_PRIVATE);
+        String pin = prefs.getString("clave_pin", null);
+
+        if (pin == null) {
+            // No hay PIN → ir a configuración
+            startActivity(new Intent(this, SetPinActivity.class));
+        } else {
+            // Hay PIN → ir a validación
+            startActivity(new Intent(this, PinActivity.class));
+        }
+
+        finish(); // Cerramos esta actividad temporal
     }
 }
